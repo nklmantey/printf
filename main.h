300528db1e72a1115a39f1476387a654540f0cc9
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-08-07 20:25:52 +0000

    test

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef our_printf
 #define our_printf
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 /**
  * @struct specifier - struct specifier
@@ -8,15 +9,17 @@
  * @f: associated functions
  *
  */
-typedef struct specifier
+struct specifier
 {
 	char *pass;
 	int (*f)(va_list);
-} spec;
+};
+typedef struct specifier spec;
+
 int _printf(const char *format, ...);
-int handle_c(va_list args);
-int handle_s(va_list args);
-int handle_percent(va_list args);
-int _putchar(char c);
+int handle_c(va_list);
+int handle_s(va_list);
+int handle_percent(va_list);
+int _putchar(char);
 int (*get_functions(char x))(va_list args);
 #endif
